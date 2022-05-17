6ffa564f4cf1c0f0639e2b0984a41b2b065e7598
RespaldoGIT <automatizacion@ecopetrol.com.co>
2022-05-17 10:27:14 -0500

    add

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -18,7 +18,7 @@ for fact in r_obj_list:
 # Select a random fact from the fact_list and return it
 # into a variable named random_fact so we can use it
 def select_random_fact(fact_arr):
-    return fact_arr[random.randint(0, len(fact_list)+1)]
+    return fact_arr[random.randint(0, len(fact_list)-1)]
 
 random_fact = select_random_fact(fact_list)
 
